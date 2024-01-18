b8bf6b86c586e9bb9ff37e66b154494988ae47dc
tarun <atarun19@gmail.com>
2024-01-18 18:44:14 +0530

    Update sample_producer.py

--- example/python/sample_producer.py
+++ example/python/sample_producer.py
@@ -5,8 +5,10 @@ The provided Kafka Datasource visualizes those samples in Grafana.
 
 from confluent_kafka import Producer
 from time import sleep
-from random import random
+import random
 import json
+import datetime
+
 
 conf = {'bootstrap.servers': 'localhost:9092'}
 producer = Producer(conf)
@@ -14,10 +16,16 @@ producer = Producer(conf)
 counter = 1
 
 while True:
-    data = {"value1": random(),
-            "value2": 1 + random()}
+    ran = random.randint(0, 12) 
+    x = datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=-ran))).strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+    data = {"value": random.randint(100, 200) ,
+            "name": "name" + str(random.random()),
+            "quality": "quality"+ str(random.random()),
+            "valuetimestamp": x}        
     producer.produce("test", value=json.dumps(data))
     print("Sample #{} produced!".format(counter))
+    print("time #{} produced!".format(x))
+    print("time #{} produced!".format(random.random()))
     counter += 1
     producer.flush(1)
     sleep(0.5)
